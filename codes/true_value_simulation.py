3e5fad7667a100e5d704b4b951735a888867241a
Arsalan Yavari <arya48.yavari79@gmail.com>
2023-11-20 14:59:22 +0330

    merge both files

--- codes/true_value_simulation.py
+++ codes/true_value_simulation.py
@@ -1,5 +1,6 @@
 import re
-
+from gates import *
+from ansii_colors import bcolors
 
 class Circuit:
     def __init__(self):
@@ -7,18 +8,23 @@ class Circuit:
         self.outputs = []
         self.gates = []
 
-    def set_inputs(self, _inputs): self.inputs = _inputs
+    def set_inputs(self, _inputs): 
+        self.inputs = _inputs
 
-    def get_inputs(self): return self.inputs
+    def get_inputs(self): 
+        return self.inputs
 
-    def set_outputs(self, _outputs): self.outputs = _outputs
+    def set_outputs(self, _outputs): 
+        self.outputs = _outputs
 
-    def get_outputs(self): return self.outputs
+    def get_outputs(self): 
+        return self.outputs
 
-    def set_gates(self, _gates): self.gates = _gates
+    def set_gates(self, _gates): 
+        self.gates = _gates
 
-    def get_gates(self): return self.gates
-
+    def get_gates(self): 
+        return self.gates
 
 class Gate:
     def __init__(self):
@@ -26,19 +32,24 @@ class Gate:
         self.gate_output = {}
         self.gate_type = ''
 
-    def set_inputs(self, _inputs): self.gate_inputs = _inputs
+    def set_inputs(self, _inputs): 
+        self.gate_inputs = _inputs
 
-    def get_inputs(self): return self.gate_inputs
+    def get_inputs(self): 
+        return self.gate_inputs
 
-    def set_output(self, _output): self.gate_output = _output
+    def set_output(self, _output): 
+        self.gate_output = _output
 
-    def get_output(self): return self.gate_output
+    def get_output(self): 
+        return self.gate_output
 
-    def set_gate_type(self, _gate_type): self.gate_type = _gate_type
+    def set_gate_type(self, _gate_type): 
+        self.gate_type = _gate_type
 
-    def get_gate_type(self): return self.gate_type
-
-
+    def get_gate_type(self): 
+        return self.gate_type
+    
 def get_file(file_name):
     file_path = "../bench files/" + file_name + ".bench"
     code = []
@@ -52,8 +63,7 @@ def get_file(file_name):
             code.append(line)
     return code
 
-
-def construct_circuit(code):
+def construct_circuit(code, user_input=[]):
     circuit = Circuit()
 
     circuit_inputs = []
@@ -63,8 +73,16 @@ def construct_circuit(code):
     for i in range(len(code)):
         if code[i].startswith('INPUT'):
             input_wire_name = re.findall(r'\d+', code[i])
-            input_wire_value = input('please enter value for ' + input_wire_name[0] + ' : ')
-            # todo : unaccepted values not handled
+            if user_input == []:
+                input_wire_value = input('please enter value for ' + input_wire_name[0] + ' : ')
+            else:
+                index = -1
+                for line_number in user_input:
+                    index += 1
+                    if line_number[0] == input_wire_name[0]:
+                        input_wire_value = user_input[index][1]
+                        break
+
             circuit_inputs.append([input_wire_name[0], int(input_wire_value)])
 
         elif code[i].startswith('OUTPUT'):
@@ -98,104 +116,67 @@ def construct_circuit(code):
 
     return circuit
 
-
-def gate_function(gate_type, input_0_1_count):
+def gate_function(gate_type, input_list):
     if gate_type == 'AND':
-        if input_0_1_count[0] > 0:
-            return 0
-        else:
-            return 1
+        return and_gate(input_list)
 
     if gate_type == 'NAND':
-        if input_0_1_count[1] > 0:
-            return 0
-        else:
-            return 1
+        return nand_gate(input_list)
 
     if gate_type == 'OR':
-        if input_0_1_count[1] > 0:
-            return 1
-        else:
-            return 0
+        return or_gate(input_list)
 
     if gate_type == 'NOR':
-        if input_0_1_count[1] > 0:
-            return 0
-        else:
-            return 1
+        return nor_gate(input_list)
 
     if gate_type == 'XOR':
-        if input_0_1_count[1] % 2 == 1:
-            return 1
-        elif input_0_1_count[1] % 2 == 0:
-            return 0
+        return xor_gate(input_list)
 
     if gate_type == 'XNOR':
-        if input_0_1_count[1] % 2 == 0:
-            return 1
-        elif input_0_1_count[1] % 2 == 1:
-            return 0
+        return xnor_gate(input_list)
 
     if gate_type == 'NOT':
-        if input_0_1_count[0] > 0:
-            return 1
-        elif input_0_1_count[1] > 0:
-            return 0
+        return not_gate(input_list)
 
     if gate_type == 'BUFFER':
-        if input_0_1_count[0] > 0:
-            return 0
-        elif input_0_1_count[1] > 0:
-            return 1
-
+        return buffer_gate(input_list)
 
 def evaluate_circuit(circuit):
     gates = circuit.get_gates()
     inputs = circuit.get_inputs()
+    
+    # TODO: initializer
     updated_gates = []
-
     for gate in gates:
         gate_inputs = gate.get_inputs()
         for gate_input_index in range(len(gate_inputs)):
-            for input in inputs:
-                if input[0] == gate_inputs[gate_input_index][0]:
-                    gate_inputs[gate_input_index][1] = input[1]
+            for g_input in inputs:
+                if g_input[0] == gate_inputs[gate_input_index][0]:
+                    gate_inputs[gate_input_index][1] = g_input[1]
                     gate.set_inputs(gate_inputs)
         updated_gates.append(gate)
 
     circuit.set_gates(updated_gates)
 
-    iterator = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]  # todo : figure out iteration value
-    for _ in iterator:
+    iterator = len(circuit.get_gates())
+    for _ in range(iterator):
+        input_value_list = []
         for gate in gates:
             gate_inputs = gate.get_inputs()
-            gate_0_1_input_count = [0, 0, '']
             gate_output = gate.get_output()
             gate_type = gate.get_gate_type()
-            for input in gate_inputs:
-                if input[1] == 'U':
-                    gate_0_1_input_count[2] = 'U'
-                    gate_output[1] = 'U'
-                elif input[1] == 'Z':
-                    gate_0_1_input_count[2] = 'Z'
-                    gate_output[1] = 'Z'
-                elif input[1] == 0:
-                    gate_0_1_input_count[2] = ''
-                    gate_0_1_input_count[0] += 1
-                elif input[1] == 1:
-                    gate_0_1_input_count[2] = ''
-                    gate_0_1_input_count[1] += 1
+            for gate_input in gate_inputs:
+                input_value_list.append(gate_input[1])
 
-            if gate_0_1_input_count[2] != 'U' and gate_0_1_input_count[2] != 'Z':
-                gate_output[1] = gate_function(gate_type[0], gate_0_1_input_count)
-
+            gate_output[1] = gate_function(gate_type[0], input_value_list)
+            
             gate.set_output(gate_output)
 
         gate_outputs = []
         for gate in gates:
             gate_outputs.append(gate.get_output())
 
-        for gate in gates:
+        for gate in gates:                              # TODO: makes it function
             gate_inputs = gate.get_inputs()
             for index in range(len(gate_inputs)):
                 for gate_output in gate_outputs:
@@ -214,15 +195,47 @@ def evaluate_circuit(circuit):
         circuit.set_outputs(circuit_outputs)
 
 
+def true_value_simulation():
+    pass
+
 def main():
-    file_name = input('please enter file name : ')  # todo : unaccepted file name not handled
+    # file_name = input("Please enter the bench file name: ")
+    file_name = "c17"
     code = get_file(file_name)
 
-    circuit = construct_circuit(code)
+    # wire_names = input(bcolors.GREEN + "Please enter the wires names >> " + bcolors.PROMPT)
+    # wire_values = input(bcolors.RED + "Please enter each wire value >>" + bcolors.PROMPT)
 
-    evaluate_circuit(circuit)
+    user_input = [["1",1],["3",0],["6",0],["2",1],["7",1]]
+    circuit = construct_circuit(code, user_input)
+
+    print(circuit.get_inputs())
+    print("")
+    print("")
+    print("")
+    for gate in circuit.get_gates():
+        print(gate.get_gate_type())
+        print(gate.get_inputs())
+        print(gate.get_output())
+        print("")
 
     print(circuit.get_outputs())
 
+    evaluate_circuit(circuit)
 
-main()
+    print("--------------------------")
+
+    print(circuit.get_inputs())
+    print("")
+    print("")
+    print("")
+    for gate in circuit.get_gates():
+        print(gate.get_gate_type())
+        print(gate.get_inputs())
+        print(gate.get_output())
+        print("")
+
+    print(circuit.get_outputs())
+
+if __name__ == "__main__":
+    main()
